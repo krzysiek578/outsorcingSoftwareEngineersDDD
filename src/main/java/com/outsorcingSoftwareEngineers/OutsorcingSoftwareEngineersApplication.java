928ee0b04f4c0aac514626022dbd1a690b6c7f3b
krzysiek578 <krispil@o2.pl>
2023-03-20 11:08:51 +0100

    first commit

---
+++ src/main/java/com/outsorcingSoftwareEngineers/OutsorcingSoftwareEngineersApplication.java
@@ -0,0 +1,13 @@
+package com.outsorcingSoftwareEngineers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsorcingSoftwareEngineersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutsorcingSoftwareEngineersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outsorcingSoftwareEngineers/OutsorcingSoftwareEngineersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outsorcingSoftwareEngineers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsorcingSoftwareEngineersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
